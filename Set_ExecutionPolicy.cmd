75748173e4ac7c81e9ec2d45fdb8543d1781aa8b
thuhollow2 <131473776+thuhollow2@users.noreply.github.com>
2024-02-29 03:49:40 +0800

    Add files via upload

--- Set_ExecutionPolicy.cmd
+++ Set_ExecutionPolicy.cmd
@@ -1,8 +1,28 @@
 @echo off
+net session >nul 2>&1
+if %errorlevel% == 0 ( goto :admin )
 
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /b
+
+:admin
 for /f "delims=" %%i in ('powershell -command "Get-ExecutionPolicy -Scope CurrentUser"') do set policy=%%i
 
 if "%policy%" NEQ "RemoteSigned" (
-    echo powershell -command "Set-ExecutionPolicy -ExecutionPolicy %policy% -Scope CurrentUser" > Remove_ExecutionPolicy.cmd
+    (
+        echo @echo off
+        echo net session ^>nul 2^>^&1
+        echo if %%errorlevel%% == 0 ^( goto :admin ^)
+        echo.
+        echo echo Set UAC = CreateObject^^^("Shell.Application"^^^) ^> "%%temp%%\getadmin.vbs"
+        echo echo UAC.ShellExecute "%%~s0", "", "", "runas", 1 ^>^> "%%temp%%\getadmin.vbs"
+        echo "%%temp%%\getadmin.vbs"
+        echo exit /b
+        echo.
+        echo :admin
+        echo powershell -command "Set-ExecutionPolicy -ExecutionPolicy %policy% -Scope CurrentUser"
+    ) > "%~dp0Remove_ExecutionPolicy.cmd"
     powershell -command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser"
 )
